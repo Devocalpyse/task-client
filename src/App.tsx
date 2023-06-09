2292cdcb2d1f823039ba99efc81a928d53673db5
Devocalpyse <ngl2d9uvg@relay.firefox.com>
2023-06-09 10:36:37 -0700

    Added base app routes / starter TaskList page (empty, to do)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import 'dotenv/config';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -28,12 +28,8 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
+        <Route exact path='/tasks' />
+        <Redirect exact from='/' to='/tasks' />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
---
+++ src/pages/TaskList.tsx
@@ -0,0 +1,11 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
+const TaskList: React.FC = () => {
+  return (
+    <IonPage>
+      
+    </IonPage>
+  );
+};
+
+export default TaskList;
