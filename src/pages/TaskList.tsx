ec97e56b1301b5807a590697f98d61a1810f1fa5
Devocalpyse <ngl2d9uvg@relay.firefox.com>
2023-06-10 20:32:36 -0700

    Task provider tested - useEffect() successively grabs tasks from API

--- src/pages/TaskList.tsx
+++ src/pages/TaskList.tsx
@@ -3,7 +3,7 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/rea
 const TaskList: React.FC = () => {
   return (
     <IonPage>
-      
+      <h1>Some content</h1>
     </IonPage>
   );
 };
