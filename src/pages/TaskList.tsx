55b7f7f4d30db79688d402ffabbd0e65ad46c64b
Devocalpyse <ngl2d9uvg@relay.firefox.com>
2023-06-10 21:15:42 -0700

    Confirmed that DialogButton does indeed create a new task on the back end (checked w/ Postman)

--- src/pages/TaskList.tsx
+++ src/pages/TaskList.tsx
@@ -1,9 +1,11 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import DialogButton from '../components/DialogButton';
 
 const TaskList: React.FC = () => {
   return (
     <IonPage>
       <h1>Some content</h1>
+      <DialogButton />
     </IonPage>
   );
 };
